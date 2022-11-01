c0119b2dbb7bbba7bfc9c5e398a931d88b2e5820
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-01 17:54:31 -0300

    feat: create load component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,16 +1,24 @@
-import { StatusBar } from "expo-status-bar";
-import { NativeBaseProvider, Center, Text } from "native-base";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { Signin } from "./src/screens/Signin";
+import { Loading } from "./src/components/Loading";
 import { THEME } from "./src/styles/theme";
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor={"gray.900"}>
-        <Text color={"white"} fontSize={24}>
-          Hello, NLW Copa
-        </Text>
-        <StatusBar style="auto" />
-      </Center>
+      {fontLoaded ? <Signin /> : <Loading />}
     </NativeBaseProvider>
   );
 }
