eebbaf7c5a71b38b7680cf4d0758f65ad38075bb
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-03 14:34:05 -0300

    feat: google auth

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
+import * as Google from "expo-auth-session/providers/google";
+import * as AuthSession from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
@@ -8,6 +13,7 @@ interface UserProps {
 export interface AuthContextDataProps {
   user: UserProps;
   signIn: () => Promise<void>;
+  isUserLoading: boolean;
 }
 
 export interface AuthContextProviderProps {
@@ -17,18 +23,42 @@ export interface AuthContextProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [isUserLoading, setIsUserLoading] = useState(false);
+  const [user, setUser] = useState<UserProps>({} as UserProps);
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: "",
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ["profile", "email"],
+  });
+
   async function signIn() {
-    console.log("Vamos Logar!");
+    try {
+      setIsUserLoading(true);
+      await promptAsync();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
   }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log("TOKEN DE AUTENTICAÇÃO ===>", access_token);
+  }
+
+  useEffect(() => {
+    if (response?.type === "success" && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
 
   return (
     <AuthContext.Provider
       value={{
         signIn,
-        user: {
-          name: "Pedro Mascarenhas",
-          avatartUrl: "https://github.com/pedro-drosa.png",
-        },
+        isUserLoading,
+        user,
       }}
     >
       {children}
--- mobile/src/screens/Signin.tsx
+++ mobile/src/screens/Signin.tsx
@@ -1,9 +1,12 @@
 import { Center, Icon, Text } from "native-base";
 import { Fontisto } from "@expo/vector-icons";
+import { useAuth } from "../hooks/useAuth";
 import Logo from "../assets/logo.svg";
 import { Button } from "../components/Button";
 
 export function Signin() {
+  const { signIn, user } = useAuth();
+
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
@@ -12,6 +15,7 @@ export function Signin() {
         title="ENTRAR COM O GOOGLE"
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
         mt={12}
+        onPress={signIn}
       />
       <Text color="white" textAlign="center" mt={4}>
         Não utilizamos nenhuma informação além {"\n"}
