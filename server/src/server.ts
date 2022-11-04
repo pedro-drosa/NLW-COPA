e7b4edba69b8cf07e3b02497e3ea8aa23d7b984b
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-04 10:32:36 -0300

    feat(auth.ts): jwt authentication flow

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
 
 import { poolRoutes } from "./routes/pool";
 import { userRoutes } from "./routes/user";
 import { guessRoutes } from "./routes/guess";
+import { authRoutes } from "./routes/auth";
 
 async function bootstrap() {
   const fastify = Fastify({ logger: true });
@@ -12,9 +14,14 @@ async function bootstrap() {
     origin: true,
   });
 
+  await fastify.register(require("@fastify/jwt"), {
+    secret: "supersecret",
+  });
+
   await fastify.register(poolRoutes);
   await fastify.register(userRoutes);
   await fastify.register(guessRoutes);
+  await fastify.register(authRoutes);
 
   await fastify.listen({ port: 3333 });
 }
